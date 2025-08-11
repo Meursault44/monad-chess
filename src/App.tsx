48a31a9181bc7d0b50822dab292c56bf7ac567c2
Meursault <47122698+Meursault44@users.noreply.github.com>
2025-08-11 16:20:26 +0300

    add pieces

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 import { Chessboard, defaultPieces } from 'react-chessboard';
 import { useRef, useState } from 'react';
 import { Chess } from 'chess.js'
-import wP from './assets/BillMonday.png'
-import bP from './assets/Keone.png'
-import wN from './assets/whiteKnight.png'
-import bN from './assets/blackKnight.png'
-import wB from './assets/whiteBishop.png'
-import bB from './assets/blackBishop.png'
+import wP from './assets/wP.png'
+import bP from './assets/bP.png'
+import wN from './assets/wN.png'
+import bN from './assets/bN.png'
+import wB from './assets/wB.png'
+import bB from './assets/bB.png'
+import wK from './assets/mikeWK.png'
+import bK from './assets/bK.png'
+import wR from './assets/wR.png'
+import bR from './assets/bR.png'
 
 function App() {
     const chessGameRef = useRef(new Chess());
@@ -16,12 +20,16 @@ function App() {
     const customPieces = {
         ...defaultPieces,
         // exported from react-chessboard
-        wP: () => <img src={wP} style={{width: '70%'}} alt="white Pawn"/>,
-        bP: () => <img src={bP} style={{width: '70%'}} alt="black Pawn"/>,
+        wP: () => <img src={wP} style={{width: '80%', marginTop: '8px'}} alt="white Pawn"/>,
+        bP: () => <img src={bP} style={{width: '80%', marginTop: '8px'}} alt="black Pawn"/>,
         wN: () => <img src={wN} style={{width: '105%'}} alt="white knight"/>,
         bN: () => <img src={bN} style={{width: '105%'}} alt="black knight"/>,
-        wB: () => <img src={wB} style={{width: '65%'}} alt="white bishop"/>,
-        bB: () => <img src={bB} style={{width: '65%'}} alt="blac bishop"/>,
+        wB: () => <img src={wB} style={{width: '90%', marginTop: '3px'}} alt="white bishop"/>,
+        bB: () => <img src={bB} style={{width: '90%', marginTop: '3px'}} alt="blac bishop"/>,
+        wK: () => <img src={wK} style={{width: '72%'}} alt="white king"/>,
+        bK: () => <img src={bK} style={{width: '82%'}} alt="black king"/>,
+        wR: () => <img src={wR} style={{width: '90%', marginLeft: '8px', marginTop: '8px'}} alt="white rook"/>,
+        bR: () => <img src={bR} style={{width: '90%', marginLeft: '8px', marginTop: '8px'}} alt="black rook"/>,
     };
     // track the current position of the chess game in state to trigger a re-render of the chessboard
     const [chessPosition, setChessPosition] = useState(chessGame.fen());
