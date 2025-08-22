f8e525f4d920d0d4f7b3ab83a648b8924ecf5514
Meursault <47122698+Meursault44@users.noreply.github.com>
2025-08-22 17:27:48 +0300

    puzzle by rating

--- src/pages/PuzzlesPage.tsx
+++ src/pages/PuzzlesPage.tsx
@@ -4,7 +4,7 @@ import ChessBoardWrapper from '@/components/ChessBoardWrapper.tsx';
 import { Button } from '@chakra-ui/react';
 import { usePuzzleEngine } from '@/hooks/usePuzzleEngine';
 import { useChessStore } from '@/store/chess.ts';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDialogsStore } from '@/store/dialogs.ts';
 
 function sideToMoveFromFen(fen?: string): 'w' | 'b' | '' {
@@ -15,7 +15,7 @@ function sideToMoveFromFen(fen?: string): 'w' | 'b' | '' {
 
 export const PuzzlesPage = () => {
   const { data: puzzle, refetch } = useQuery({
-    queryKey: ['puzzle'],
+    queryKey: ['puzzle', { minRating: 800, maxRating: 900 }],
     queryFn: getRandomPuzzle,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
@@ -23,11 +23,13 @@ export const PuzzlesPage = () => {
     staleTime: Infinity, // данные не «протухают»
     gcTime: Infinity, // не удаляются из кэша
   });
+
   const side = sideToMoveFromFen(puzzle?.fen);
   const { validateMove, opponentLogic, isSolved, onPlayerAccepted } = usePuzzleEngine(puzzle, side);
   const { setDialogSolvedPuzzle } = useDialogsStore();
 
   const startFromFen = useChessStore((s) => s.startFromFen);
+  const phase = useChessStore((s) => s.phase);
   const resetGame = useChessStore((s) => s.resetGame);
 
   useEffect(() => {
@@ -41,34 +43,41 @@ export const PuzzlesPage = () => {
   }, [isSolved]);
 
   return (
-    <div className="flex w-[1070px] gap-6">
+    <div className="flex w-[1170px] gap-6">
       <ChessBoardWrapper
         onOpponentTurn={opponentLogic}
         validateMove={validateMove}
         onPlayerAccepted={onPlayerAccepted}
       />
-      <Button
-        onClick={() => {
-          if (!puzzle?.fen) return;
-          const sideNow = sideToMoveFromFen(puzzle.fen);
-          startFromFen(puzzle.fen, sideNow);
-        }}
-      >
-        Start
-      </Button>
-      <Button
-        onClick={async () => {
-          const res = await refetch(); // дождались новые данные
-          const next = res.data;
-          if (next?.fen) {
-            const sideNext = sideToMoveFromFen(next.fen);
-            startFromFen(next.fen, sideNext); // теперь стартуем именно НОВЫЙ пазл
-          }
-        }}
-      >
-        Next Puzzle
-      </Button>
-      <div className={'text-white'}>{side === 'w' ? 'White move' : 'Black move'}</div>
+      <div className="my-auto flex h-[860px] w-[300px] shrink-0 flex-col bg-[#4E3371]">
+        {phase === 'idle' ? (
+          <Button
+            onClick={() => {
+              if (!puzzle?.fen) return;
+              const sideNow = sideToMoveFromFen(puzzle.fen);
+              startFromFen(puzzle.fen, sideNow);
+            }}
+          >
+            Start
+          </Button>
+        ) : (
+          <>
+            <Button
+              onClick={async () => {
+                const res = await refetch(); // дождались новые данные
+                const next = res.data;
+                if (next?.fen) {
+                  const sideNext = sideToMoveFromFen(next.fen);
+                  startFromFen(next.fen, sideNext); // теперь стартуем именно НОВЫЙ пазл
+                }
+              }}
+            >
+              Next Puzzle
+            </Button>
+            <div className={'text-white'}>{side === 'w' ? 'White move' : 'Black move'}</div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
